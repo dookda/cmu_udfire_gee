12f85af8a6b7b5e043bac704a178f26f9762f4d8
dookda <sakda.homhuan@gmail.com>
2025-09-05 12:50:59 +0700

    update new code

---
+++ hotpred/da.py
@@ -0,0 +1,6 @@
+def main1(study_area=None):
+    if study_area is None:
+        print("กำลังเริ่มต้น Google Earth Engine...")
+    else:
+        print(
+            f"กำลังเริ่มต้น Google Earth Engine สำหรับพื้นที่ศึกษา: {study_area}...")
